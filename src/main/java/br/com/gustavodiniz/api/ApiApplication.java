4d85f2b3df0f83baecf14914caa9a71a7edfa0f0
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-05-26 11:22:20 -0300

    create UserModel

--- src/main/java/br/com/gustavodiniz/api/ApiApplication.java
+++ src/main/java/br/com/gustavodiniz/api/ApiApplication.java
@@ -1,13 +1,14 @@
 package br.com.gustavodiniz.api;
 
+import br.com.gustavodiniz.api.models.UserModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 
 }
---
+++ src/main/java/br/com/gustavodiniz/api/models/UserModel.java
@@ -0,0 +1,26 @@
+package br.com.gustavodiniz.api.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "TB_USERS")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}
