5615ea5604e9d7235197e406c52abe23b48a1551
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-05-26 12:23:39 -0300

    create UserController

---
+++ src/main/java/br/com/gustavodiniz/api/controllers/UserController.java
@@ -0,0 +1,23 @@
+package br.com.gustavodiniz.api.controllers;
+
+import br.com.gustavodiniz.api.models.UserModel;
+import br.com.gustavodiniz.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserModel> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(userService.findById(id));
+    }
+}
