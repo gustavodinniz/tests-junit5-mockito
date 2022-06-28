68b4d21469e6ee6f80cc2813dd7a8ba48442aab7
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-06-28 11:12:21 -0300

    finalizing project with 100% coverage in tests

--- src/test/java/br/com/gustavodiniz/api/controllers/exceptions/ControllerExceptionHandlerTest.java
+++ src/test/java/br/com/gustavodiniz/api/controllers/exceptions/ControllerExceptionHandlerTest.java
@@ -1,5 +1,6 @@
 package br.com.gustavodiniz.api.controllers.exceptions;
 
+import br.com.gustavodiniz.api.services.exceptions.DataIntegrityViolationException;
 import br.com.gustavodiniz.api.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -37,9 +40,26 @@ class ControllerExceptionHandlerTest {
         assertEquals(StandardError.class, response.getBody().getClass());
         assertEquals("User not found.", response.getBody().getError());
         assertEquals(404, response.getBody().getStatus());
+
+        assertNotEquals("/user/2", response.getBody().getPath());
+        assertNotEquals(LocalDateTime.now(), response.getBody().getTimestamp());
     }
 
     @Test
     void dataIntegrityViolation() {
+        ResponseEntity<StandardError> response = exceptionHandler
+                .dataIntegrityViolation(
+                        new DataIntegrityViolationException("E-mail already registered in the system."),
+                        new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals( "E-mail already registered in the system.", response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
+
+
     }
 }
