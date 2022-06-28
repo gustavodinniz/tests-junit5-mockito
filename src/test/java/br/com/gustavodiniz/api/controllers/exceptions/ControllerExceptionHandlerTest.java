d59a4a89cf0044b801b10fa0b2db8a643369edcd
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-06-28 10:58:27 -0300

    implementing test for exceptionHandler method objectNotFound

---
+++ src/test/java/br/com/gustavodiniz/api/controllers/exceptions/ControllerExceptionHandlerTest.java
@@ -0,0 +1,45 @@
+package br.com.gustavodiniz.api.controllers.exceptions;
+
+import br.com.gustavodiniz.api.services.exceptions.ObjectNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ControllerExceptionHandlerTest {
+
+    @InjectMocks
+    private ControllerExceptionHandler exceptionHandler;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void whenObjectNotFoundExceptionThenReturnAResponseEntity() {
+        ResponseEntity<StandardError> response = exceptionHandler
+                .objectNotFound(
+                        new ObjectNotFoundException("User not found."),
+                        new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals("User not found.", response.getBody().getError());
+        assertEquals(404, response.getBody().getStatus());
+    }
+
+    @Test
+    void dataIntegrityViolation() {
+    }
+}
